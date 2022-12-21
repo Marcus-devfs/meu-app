0a2d022d0c05d9ed405481802176bdaa5bf73b9b
Marcus-devfs <marcusvini6277@gmail.com>
2022-12-21 18:21:23 -0300

    alterações aba de movimentações

---
+++ src/context/validadores.js
@@ -0,0 +1,24 @@
+export const formatDate = ({ date = null, birthday = false, showTime = false, showSeconds = true }) => {
+    if (date) {
+      let newTimestamp = new Date(date)
+
+      if (!birthday) {
+        let timezoneDifference = newTimestamp.getTimezoneOffset()
+        newTimestamp.setTime(newTimestamp.getTime() - (timezoneDifference * 60 * 1000))
+      }
+
+      let splittedTimestamp = newTimestamp.toJSON().split('T')
+      let formatDate = splittedTimestamp[0].split('-')
+      let formatedTime = splittedTimestamp[1].split('.')
+      formatDate = `${formatDate[2]}/${formatDate[1]}/${formatDate[0]}`
+      formatedTime = formatedTime[0]
+
+      if (!showSeconds) {
+        let splittedTime = formatedTime.split(":")
+        formatedTime = `${splittedTime[0]}:${splittedTime[1]}`
+      }
+
+      return showTime ? `${formatDate} às ${formatedTime}` : formatDate
+    }
+    return `-`
+  }
